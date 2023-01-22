f29bc7dbd0b5659fcd53a0551b7e0bcb50871250
ShubhamPalriwala <spalriwalau@gmail.com>
2023-01-23 01:49:10 +0530

    feat: create acc if no key is passed on deployment

--- main.py
+++ main.py
@@ -61,12 +61,22 @@ def create_account_api():
 
 @app.post("/deploy_contract")
 def deploy_to_chain_api(contract: ContractToDeploy):
-    deploy_contract_proc = subprocess.run(["sh", "./utils/deploy-contract.sh", contract.lib_file, contract.secret_key],
+
+    if len(contract.secret_key) == 0:
+        acc_created, secret, pub_key, message = create_account()
+        if not acc_created:
+            return {"success": False, "contract_id": None, "message": "Please create an account manually via /create_account and try again!"}
+
+        secret_key = secret
+    else:
+        secret_key = contract.secret_key
+
+    deploy_contract_proc = subprocess.run(["sh", "./utils/deploy-contract.sh", contract.lib_file, secret_key],
                                           stdout=subprocess.PIPE, universal_newlines=True)
 
     contract_id = deploy_contract_proc.stdout
 
     if contract_id != '':
-        return {"success": True, "contract_id": contract_id, "message": "Succesfully deployed contract!"}
+        return {"success": True, "contract_id": contract_id, "message": "Succesfully deployed contract!", "secret_seed": secret, "pub_key": pub_key}
 
     return {"success": False, "contract_id": None, "message": "Something went wrong!"}
