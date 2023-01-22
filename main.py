5d1ac3a7e0928f13724f5fe8fb501e15cf2b5286
Hemanth Krishna <hkpdev008@gmail.com>
2023-01-23 02:03:53 +0530

    feat: invoke contract
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class ContractToDeploy(BaseModel):
     lib_file: str
     secret_key: str
 
+class ContractToInvoke(BaseModel):
+    contract_id: str
+    contract_function: str
+    secret_key: str
+    contract_arguments: list[str]
 
 class Contract(BaseModel):
     lib_file: str
@@ -96,3 +101,21 @@ def compile_contract_api(contract: ContractToDeploy):
 
     error_message = wasm_compilation_proc.stderr
     return {"success": False, "message": error_message}
+    
+@app.post("/invoke_contract")
+def invoke_contract_api(contract: ContractToInvoke):
+    base_call = f"soroban invoke \
+    --id {contract.contract_id} \
+    --secret-key {contract.secret_key} \
+    --rpc-url http://localhost:8000/soroban/rpc \
+    --network-passphrase 'Test SDF Future Network ; October 2022' \
+    --fn {contract.contract_function} \
+    "
+    for arg in contract.contract_arguments:
+        base_call += f"--arg {arg} "
+    invoke_contract_proc = subprocess.run([base_call],shell=True ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if invoke_contract_proc.stdout != '':
+        return {"success": True, "results": invoke_contract_proc.stdout}
+    else:
+        return {"success": False, "results": invoke_contract_proc.stderr}
+
