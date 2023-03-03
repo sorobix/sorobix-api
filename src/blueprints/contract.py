19df16e6e43cb518fee5272856d96a1e382fdb22
ShubhamPalriwala <spalriwalau@gmail.com>
2023-03-03 16:35:27 +0530

    fix: compile contract

--- src/blueprints/contract.py
+++ src/blueprints/contract.py
@@ -4,21 +4,22 @@ from src.services.contract_deployment import deploy_to_chain_api
 from src.services.contract_compilation import compile_contract_api
 from src.services.contract_invokation import invoke_contract_api
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 contract_blueprint = Blueprint(name="contract_blueprint", import_name=__name__)
 
 
-@contract_blueprint.route('/deploy', methods=['POST'])
+@contract_blueprint.route('/compile', methods=['POST'])
+def compile_contract_blueprint():
+    lib_file = request.json["lib_file"]
+    return compile_contract_api(lib_file=lib_file)
+
+
+@ contract_blueprint.route('/deploy', methods=['POST'])
 def deploy_to_chain_blueprint(contract: ContractToDeploy):
     return deploy_to_chain_api(contract)
 
 
-@contract_blueprint.route('/compile', methods=['POST'])
-def compile_contract_blueprint(contract: ContractToDeploy):
-    return compile_contract_api(contract)
-
-
-@contract_blueprint.route('/invoke', methods=['POST'])
+@ contract_blueprint.route('/invoke', methods=['POST'])
 def invoke_contract_api(contract: ContractToInvoke):
     return invoke_contract_api(contract)
--- src/services/contract_invokation.py
+++ src/services/contract_invokation.py
@@ -1,5 +1,5 @@
-from src.models.invoke import ContractToInvoke
-from utils.soroban_client import prepare_soroban, call_soroban
+from ..models.invoke import ContractToInvoke
+from ..utils.soroban_client import prepare_soroban, call_soroban
 
 
 def invoke_contract_api(contract: ContractToInvoke):
