3f5191873ce0d9d0bc3d23841c6109fd891aeef8
Hemanth Krishna <hkpdev008@gmail.com>
2023-03-13 01:12:11 +0530

    feat: add Dockerfile
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- wsgi.py
+++ wsgi.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     ####################
     # FOR DEVELOPMENT
     ####################
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port='3001', debug=True)
