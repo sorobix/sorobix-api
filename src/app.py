0ccbb34200b9b4530d861b6a8072d82a28d9a36e
ShubhamPalriwala <spalriwalau@gmail.com>
2023-03-13 02:23:19 +0530

    feat: cors

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from .blueprints.contract import contract_blueprint
 from .blueprints.account import account_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(account_blueprint, url_prefix="/account")
 app.register_blueprint(contract_blueprint, url_prefix="/contract")
+CORS(app)
 
 if __name__ == "__main__":
     app.run()
