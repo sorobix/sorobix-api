fe780fbaca8a0a1ab7220868c82f0ef6c8f143e6
ShubhamPalriwala <spalriwalau@gmail.com>
2023-03-12 16:35:27 +0530

    fix: compile contract

--- src/services/contract_compilation.py
+++ src/services/contract_compilation.py
@@ -1,13 +1,16 @@
 import subprocess
-from src.models.deployment import ContractToDeploy
+from pathlib import Path
 
 
-def compile_contract_api(contract: ContractToDeploy):
-    wasm_compilation_proc = subprocess.run(["sh", "./utils/compile-to-wasm.sh", contract.lib_file],
+def compile_contract_api(lib_file: str):
+    curr_dir = Path(__file__).parent
+    src_path = str(Path(curr_dir).parents[0])
+
+    wasm_compilation_proc = subprocess.run(["sh", src_path + "/utils/compile-to-wasm.sh", lib_file],
                                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     if wasm_compilation_proc.returncode == 0:
         return {"success": True, "message": "Compiled Successfully!"}
 
     error_message = wasm_compilation_proc.stderr
-    return {"success": False, "message": error_message}
+
+    return {"success": False, "message": str(error_message)}
